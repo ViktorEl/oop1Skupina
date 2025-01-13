2f6e8e52dbc190972859b30a674ba1ea5126e3f1
Your Name <you@example.com>
2025-01-13 08:57:03 +0100

    Doplnenie teorie

--- kniha.py
+++ kniha.py
@@ -6,11 +6,18 @@ class Kniha():
         self.pocet_stran = pocet_stran
 
     def parametre_knihy(self):
-        return f'Názov {self.nazov} a počet strán {self.pocet_stran}'
+        return f'Názov {self.nazov}, počet strán {self.pocet_stran}'
+
 
 
 kniha1 = Kniha('Zaklináč', 350)         # referencia na objekt/inštancia
 kniha2 = Kniha('Harry Potter', 450)
+
+#print(kniha1.parametre_knihy())         # aplikovanie metody na objekt
+print(kniha1.pocet_stran)                # atribút
+print(kniha1.nazov)
+kniha1.pocet_stran = 380
+
 
 
 
