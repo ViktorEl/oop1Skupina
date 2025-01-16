a4ccbc56e513b3c9877fc573ad502cc1556071b3
Your Name <you@example.com>
2025-01-16 10:27:57 +0100

    Vytvorenie getterov a skrytie atributov - zapúzdrenie

--- kniha.py
+++ kniha.py
@@ -2,32 +2,38 @@
 class Kniha():
 
     def __init__(self, nazov, pocet_stran):         # inicializacna metoda/konštruktor
-        self.set_nazov(nazov)                         
-        self.set_pocet_stran(pocet_stran)
+        self.__set_nazov(nazov)                         
+        self.__set_pocet_stran(pocet_stran)
 
     def parametre_knihy(self):
-        return f'Názov {self.nazov}, počet strán {self.pocet_stran}'
+        return f'Názov {self.__nazov}, počet strán {self.__pocet_stran}'
 
-    def set_nazov(self, nazov):
+    def __set_nazov(self, nazov):
         knihy = ['Zaklínač', 'Harry Potter', 'Pobrežie', 'Hory', 'Tatry']
         if nazov not in knihy:
             raise ValueError('chyba')
         else:
-            self.nazov = nazov
+            self.__nazov = nazov
 
-    def set_pocet_stran(self, pocet):
+    def __set_pocet_stran(self, pocet):
         if pocet < 5 or pocet > 1000:
             raise ValueError('chyba nesplnate pocet')
         else:
-            self.pocet_stran = pocet
+            self.__pocet_stran = pocet
+
+    def get_meno(self):
+        return self.__nazov
+
+
+    def get_pocet(self):
+        return self.__pocet_stran
 
 
 
 
 kniha1 = Kniha('Zaklínač', 50)         # referencia na objekt/inštancia
 kniha2 = Kniha('Harry Potter', 450)
-kniha1.set_nazov('zak')
-print(kniha1.nazov)
+print(kniha1.get_meno())
 
 #print(kniha1.parametre_knihy())         # aplikovanie metody na objekt
 #print(kniha1.pocet_stran)                # atribút
