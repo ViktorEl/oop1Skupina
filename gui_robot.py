27330397bcccaa4939882c8c0fcf02a10548dd37
Your Name <you@example.com>
2025-04-03 10:14:49 +0200

    gui pre robota vytvorenie gui pre pozdrav

--- gui_robot.py
+++ gui_robot.py
@@ -1,7 +1,31 @@
 from robot import Robot
 import tkinter
 
+
 class GUI(tkinter.Tk):
 
     def __init__(self):
         super().__init__()
+        self.title('Robot')
+        self.geometry('600x400')
+        self.robot = Robot('Jozo', 'Zelezo')
+        self.prostredie()
+        self.mainloop()
+
+
+    def prostredie(self):
+        tkinter.Button(self, text="Pozdrav sa", font=('Arial', 15), command=self.vypis_pozdrav).place(x=10, y=10)
+        self.pozdravenie = tkinter.StringVar()
+        tkinter.Entry(self, textvariable=self.pozdravenie, font=('Arial', 15)).place(x=150, y=15)
+
+        tkinter.Button(self, text="Vymaz", font=("Arial", 15), command=self.vymaz_pozdrav).place(x=380, y=15)
+
+
+    def vypis_pozdrav(self):
+        self.pozdravenie.set(self.robot.pozdrav())
+
+    def vymaz_pozdrav(self):
+        self.pozdravenie.set('')
+
+
+GUI()
